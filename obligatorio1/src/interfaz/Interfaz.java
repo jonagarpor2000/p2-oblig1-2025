f1ded5ed574975e8605ac1efa3a9493d80b3cd41
Jonathan Garcia <jonathan.porley15@gmail.com>
2025-04-29 00:57:56 -0300

    v0.0.1 definitiva
    Boceto primario

---
+++ obligatorio1/src/interfaz/Interfaz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfaz;
+
+/**
+ *
+ * @author Nahuel Paroldo
+ * @author Jonathan Garcia
+ */
+
+//nombre + Nroestudiante en interfaz
+public class Interfaz {
+    
+}
