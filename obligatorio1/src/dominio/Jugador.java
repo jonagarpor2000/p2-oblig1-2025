9a17b6c3b25084f3d13a5ac4e4e2d3564111725b
Jonathan Garcia <jonathan.porley15@gmail.com>
2025-05-03 18:07:23 -0300

    v0.0.4
    Menu y constructores de jugador

--- obligatorio1/src/dominio/Jugador.java
+++ obligatorio1/src/dominio/Jugador.java
@@ -10,5 +10,33 @@ package dominio;
  * @author Jonathan Garcia
  */
 public class Jugador {
+    private int puntaje;
+    private String nombre;
+    private int edad;
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
     
 }
